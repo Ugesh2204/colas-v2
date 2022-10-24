8f277f1715edd586d89b1a1941a3e2537822ca2e
ugesh <ugesh.roopchand@hotmail.com>
2022-10-24 20:11:55 +0400

    Update code

--- src/atoms/button/Recherche.js
+++ src/atoms/button/Recherche.js
@@ -10,12 +10,8 @@ function Recherche( props) {
             <path id="ic_search" d="M19.448,17.474h-1.04l-.368-.355a8.566,8.566,0,1,0-.921.921l.355.368v1.04l6.579,6.566,1.961-1.961Zm-7.9,0a5.921,5.921,0,1,1,5.921-5.921A5.913,5.913,0,0,1,11.553,17.474Z" transform="translate(-3 -3)"/>
         </svg>
 
-        <form className={ props.clickSearchBtn ? 'search__input':'hide_search'}>
-            <input type="text" />
-            <div className='underline_input'></div>
-        </form>
-
-        <p className='recherche_txt'>Recherche</p>
+        
+       
     </div>
   )
 }
